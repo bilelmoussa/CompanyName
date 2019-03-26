f97b837c1046d6671fae22793396bae7d15f253d
bilel moussa <streetvayne@outlook.fr>
2019-03-26 19:36:27 +0100

    Updates

---
+++ precache-manifest.5c8821998c42f5ed77692f82a2ebeafc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd7b2eccc9ec42c7da9a0032c2fcde29",
+    "url": "/CompanyName/static/media/clothes.cd7b2ecc.jpg"
+  },
+  {
+    "revision": "42513b055ec74fbcde0505fa8cf4eec2",
+    "url": "/CompanyName/static/media/bg3.42513b05.jpg"
+  },
+  {
+    "revision": "585b92f2e4d67362c74d603d1f1e144c",
+    "url": "/CompanyName/static/media/bg2.585b92f2.jpg"
+  },
+  {
+    "revision": "bbe5ff18a3ef2d76b7d8cf4529f5c83f",
+    "url": "/CompanyName/static/media/bg1.bbe5ff18.jpeg"
+  },
+  {
+    "revision": "d093015bd48a675b8884",
+    "url": "/CompanyName/static/js/runtime~main.b3baa908.js"
+  },
+  {
+    "revision": "411723ab41c07f7dd732",
+    "url": "/CompanyName/static/js/main.405d160f.chunk.js"
+  },
+  {
+    "revision": "45c1917cd835ef6e9418",
+    "url": "/CompanyName/static/js/2.b1a2bb8c.chunk.js"
+  },
+  {
+    "revision": "411723ab41c07f7dd732",
+    "url": "/CompanyName/static/css/main.b6c8a2d2.chunk.css"
+  },
+  {
+    "revision": "986cdd2fc52691c5e4391e542bacd969",
+    "url": "/CompanyName/index.html"
+  }
+];
